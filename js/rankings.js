f949ad7449f5ef3e828626d5f64bf6a3450217bc
ckovo <ilobubles@gmail.com>
2025-01-05 01:50:45 +0800

    1

--- js/rankings.js
+++ js/rankings.js
@@ -56,32 +56,32 @@ function showRanking(type) {
 
 const rankings = {
     speed: [
-        { name: "迷之小肥羊", score: "抗疫杯：303.18秒" },
-        { name: "上杉佐助", score: "抗疫杯：306.77秒" },
-        { name: "甘心首疾已经年", score: "抗疫杯：309.84秒" },
-        { name: "夜叉", score: "抗疫杯：318.93秒" },
-        { name: "稻香我也没忘过", score: "抗疫杯：351.70秒" }
+        { name: "迷之小肥羊", score: "记录：303.18秒" },
+        { name: "上杉佐助", score: "记录：306.77秒" },
+        { name: "甘心首疾已经年", score: "记录：309.84秒" },
+        { name: "夜叉", score: "记录：318.93秒" },
+        { name: "稻香我也没忘过", score: "记录：351.70秒" }
     ],
     ogp2: [
-        { name: "TITANXPASCAL", score: "15关（5800秒）" },
-        { name: "只想做个洒脱的人", score: "14关（6000秒）" },
-        { name: "SHE", score: "14关（7500秒）" },
-        { name: "幻舒", score: "14关（8000秒）" },
-        { name: "寻龙", score: "14关（8500秒）" }
+        { name: "TITANXPASCAL", score: "记录：15关（5800秒）" },
+        { name: "只想做个洒脱的人", score: "记录：14关（6000秒）" },
+        { name: "SHE", score: "记录：14关（7500秒）" },
+        { name: "幻舒", score: "记录：14关（8000秒）" },
+        { name: "寻龙", score: "记录：14关（8500秒）" }
     ],
     ogp1: [
-        { name: "南烟吹北枳", score: "369.61秒" },
-        { name: "余不乐", score: "418.24秒" },
-        { name: "blast", score: "465.49秒" },
-        { name: "crazydouzi", score: "514.69秒" },
-        { name: "其实我是自私鬼", score: "517.78秒" }
+        { name: "南烟吹北枳", score: "记录：369.61秒" },
+        { name: "余不乐", score: "记录：418.24秒" },
+        { name: "blast", score: "记录：465.49秒" },
+        { name: "crazydouzi", score: "记录：514.69秒" },
+        { name: "其实我是自私鬼", score: "记录：517.78秒" }
     ],
     ogp3: [
-        { name: "逢魔时王", score: "420.81秒" },
-        { name: "AE86", score: "432.43秒" },
-        { name: "皮皮皮皮基", score: "440.11秒" },
-        { name: "三川", score: "442.63秒" },
-        { name: "迷之小肥羊", score: "457.50秒" }
+        { name: "逢魔时王", score: "记录：420.81秒" },
+        { name: "AE86", score: "记录：432.43秒" },
+        { name: "皮皮皮皮基", score: "记录：440.11秒" },
+        { name: "三川", score: "记录：442.63秒" },
+        { name: "迷之小肥羊", score: "记录：457.50秒" }
     ]
 };
 
